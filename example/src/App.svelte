07148c3a77e2c0c76687d4bcdc08c4cba3a10af4
tsunaki.okubo <14939888+ski-0n0@users.noreply.github.com>
2023-08-10 11:13:29 +0900

    example

---
+++ example/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { SpButton } from "@sp-design/components-web-components/src/";
+</script>
+
+<main>
+  <sp-button></sp-button>
+  <sp-button label="なんもないやつ"></sp-button>
+  <sp-button label="disabled trueやつ" disabled={true}></sp-button>
+  <sp-button label="disabled falseやつ" disabled={false}></sp-button>
+  <sp-button label="classNameあるやつ" class="classdayo"></sp-button>
+  <sp-button label="loadingあるやつ" loading="{true}"></sp-button>
+  <sp-button label="selectedあるやつ" selected="{true}"></sp-button>
+  <sp-button label="destructive" type="destructive"></sp-button>
+  <sp-button label="appearance" appearance="fill"></sp-button>
+  <sp-button label="size" size="width160"></sp-button>
+  <sp-button label="onClickやつ" on:click={()=>(console.log("fire"))}></sp-button>
+</main>
+
+<style>
+</style>
