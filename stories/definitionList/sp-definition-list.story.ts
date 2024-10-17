8e35c6463f13f60cbfa514e70b0d5ac188a14c2d
Ittetsu Sato <ittetsusato@gmail.com>
2024-10-17 22:59:54 +0900

    definition-list作成

--- stories/definitionList/sp-definition-list.story.ts
+++ stories/definitionList/sp-definition-list.story.ts
@@ -1,14 +1,17 @@
 import "../../src/components/definitionList/sp-definition-list-dt";
 import "../../src/components/definitionList/sp-definition-list-dd";
+import "../../src/components/definitionList/sp-definition-list";
 import type { Meta, StoryObj } from "@storybook/web-components";
 import "@sp-design/token/lib/speeda-tokens.css";
 import { html } from "lit";
 
 const DefinitionList = (termText: string, definitionText: string) => html`
-  <dl>
+  <sp-definition-list>
     <sp-definition-list-dt>${termText}</sp-definition-list-dt>
     <sp-definition-list-dd>${definitionText}</sp-definition-list-dd>
-  </dl>
+    <sp-definition-list-dt>${termText}</sp-definition-list-dt>
+    <sp-definition-list-dd>${definitionText}</sp-definition-list-dd>
+  </sp-definition-list>
 `;
 
 const meta: Meta = {
