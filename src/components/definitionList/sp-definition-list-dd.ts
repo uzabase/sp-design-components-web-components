6d76c0c199956d5d3060129703c6acb0a6e6adb3
Ittetsu Sato <ittetsusato@gmail.com>
2024-10-17 22:31:26 +0900

    reset.css

--- src/components/definitionList/sp-definition-list-dd.ts
+++ src/components/definitionList/sp-definition-list-dd.ts
@@ -1,10 +1,12 @@
 // @ts-ignore
+import resetStyle from "@acab/reset.css?inline" assert { type: "css" };
+// @ts-ignore
 import foundationStyle from "../foundation.css?inline" assert { type: "css" };
 // @ts-ignore
 import spDefinitionListDdStyle from "./sp-definition-list-dd.css?inline" assert { type: "css" };
 
 const styles = new CSSStyleSheet();
-styles.replaceSync(`${foundationStyle} ${spDefinitionListDdStyle}`);
+styles.replaceSync(`${resetStyle} ${foundationStyle} ${spDefinitionListDdStyle}`);
 
 export class SpDefinitionListDd extends HTMLElement {
   #ddElement = document.createElement("dd");
