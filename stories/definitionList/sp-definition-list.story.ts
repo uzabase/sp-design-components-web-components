41d56611ea34808dd2321fb2712d15ae52da2a66
Ittetsu Sato <ittetsusato@gmail.com>
2024-10-30 23:06:49 +0900

    改行のStoryを追加

--- stories/definitionList/sp-definition-list.story.ts
+++ stories/definitionList/sp-definition-list.story.ts
@@ -5,22 +5,19 @@ import type { Meta, StoryObj } from "@storybook/web-components";
 import "@sp-design/token/lib/speeda-tokens.css";
 import { html } from "lit";
 
-const DefinitionList = (termText: string, definitionText: string) => html`
-  <sp-definition-list>
-    <sp-definition-list-dt>${termText}</sp-definition-list-dt>
-    <sp-definition-list-dd>${definitionText}</sp-definition-list-dd>
-    <sp-definition-list-dt>${termText}</sp-definition-list-dt>
-    <sp-definition-list-dd>${definitionText}</sp-definition-list-dd>
-  </sp-definition-list>
-`;
-
 const meta: Meta = {
   args: {
     termText: "Term",
     definitionText: "Definition",
   },
-  render: ({ termText, definitionText }) =>
-    DefinitionList(termText, definitionText),
+  render: ({ termText, definitionText }) => html`
+    <sp-definition-list>
+      <sp-definition-list-dt>${termText}</sp-definition-list-dt>
+      <sp-definition-list-dd>${definitionText}</sp-definition-list-dd>
+      <sp-definition-list-dt>${termText}</sp-definition-list-dt>
+      <sp-definition-list-dd>${definitionText}</sp-definition-list-dd>
+    </sp-definition-list>
+  `,
 };
 export default meta;
 
@@ -33,5 +30,11 @@ export const OverflowWrap: Story = {
     termText: "LongTermLongTermLongTermLongTerm",
     definitionText:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+};
+
+export const Break: Story = {
+  args: {
+    definitionText: html`break <br />break`,
   },
 };
