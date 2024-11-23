04e1ca844a31b88c567d70cac931c1a80a3175bc
oki07 <takashi.oki@uzabase.com>
2024-11-23 14:16:26 +0900

    ストーリーの構成を更新

--- stories/dropdownAction/sp-dropdown-action.story.ts
+++ stories/dropdownAction/sp-dropdown-action.story.ts
@@ -1,7 +1,7 @@
-import "../../src/components/dropdownAction/sp-dropdown-action";
-import type { Meta, StoryObj } from "@storybook/web-components";
 import "@sp-design/token/lib/speeda-tokens.css";
+import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
+import "../../src/components/dropdownAction/sp-dropdown-action";
 
 const meta: Meta = {
   component: "sp-dropdown-action",
@@ -12,18 +12,30 @@ export default meta;
 
 type Story = StoryObj;
 
-export const Basic: Story = {};
-
-export const WithItems: Story = {
+export const Basic: Story = {
   render: () => html`
     <sp-dropdown-action label="ダッシュボード新規作成">
-      <sp-dropdown-action-item
-        >AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</sp-dropdown-action-item
-      >
       <sp-dropdown-action-item @click=${() => alert("企業を作成")}>企業を作成</sp-dropdown-action-item>
-      <sp-dropdown-action-item>業界を作成</sp-dropdown-action-item>
-      <sp-dropdown-action-item>技術を作成</sp-dropdown-action-item>
+      <sp-dropdown-action-item @click=${() => alert("業界を作成")}>業界を作成</sp-dropdown-action-item>
     </sp-dropdown-action>
+  `,
+};
+
+export const LongText: Story = {
+  render: () => html`
+    <sp-dropdown-action label="ダッシュボード新規作成">
+      <sp-dropdown-action-item>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</sp-dropdown-action-item>
+    </sp-dropdown-action>
+  `,
+};
+
+export const RightPosition: Story = {
+  render: () => html`
+    <div style="display: flex; justify-content: end">
+      <sp-dropdown-action label="ダッシュボード新規作成" open position="right">
+        <sp-dropdown-action-item>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</sp-dropdown-action-item>
+      </sp-dropdown-action>
+    </div>
   `,
 };
 
@@ -32,32 +44,6 @@ export const Disabled: Story = {
     <sp-dropdown-action label="ダッシュボード新規作成" disabled>
       <sp-dropdown-action-item>企業を作成</sp-dropdown-action-item>
       <sp-dropdown-action-item>業界を作成</sp-dropdown-action-item>
-      <sp-dropdown-action-item>技術を作成</sp-dropdown-action-item>
     </sp-dropdown-action>
-  `,
-};
-
-export const Open: Story = {
-  render: () => html`
-    <sp-dropdown-action label="ダッシュボード新規作成" open>
-      <sp-dropdown-action-item>企業を作成</sp-dropdown-action-item>
-      <sp-dropdown-action-item>業界を作成</sp-dropdown-action-item>
-      <sp-dropdown-action-item>技術を作成</sp-dropdown-action-item>
-    </sp-dropdown-action>
-  `,
-};
-
-export const RightPosition: Story = {
-  render: () => html`
-    <div style="display: flex; justify-content: end">
-      <sp-dropdown-action label="ダッシュボード新規作成" oep position="right">
-        <sp-dropdown-action-item
-          >AAAAAAAAAAAAAAAAAAAAAAAAA</sp-dropdown-action-item
-        >
-        <sp-dropdown-action-item>企業を作成</sp-dropdown-action-item>
-        <sp-dropdown-action-item>業界を作成</sp-dropdown-action-item>
-        <sp-dropdown-action-item>技術を作成</sp-dropdown-action-item>
-      </sp-dropdown-action>
-    </div>
   `,
 };
