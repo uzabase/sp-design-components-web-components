7b2ff3a05e9e6c98bab67cb1afe52ef397fcca62
oki07 <takashi.oki@uzabase.com>
2024-12-04 20:31:02 +0900

    sp-dropdown-dialogを追加

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { SpCheckboxText } from "./components/checkbox/sp-checkbox-text";
 export { SpCheckboxList } from "./components/checkbox/sp-checkbox-list";
 export { SpIcon } from "./components/icon/sp-icon";
 export { SpRadioButtonTextGroup } from "./components/radio/sp-radio-button-text-group";
+export { SpDropdownDialog } from "./components/dropdownDialog/sp-dropdown-dialog";
