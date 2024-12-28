4a011be70f7b507cbe5a02c353d33e7a001161ee
oki07 <takashi.oki@uzabase.com>
2024-12-29 02:10:03 +0900

    リファクタリング

--- src/components/pagination/sp-pagination.ts
+++ src/components/pagination/sp-pagination.ts
@@ -84,13 +84,8 @@ export class SpPagination extends HTMLElement {
   #updatePageGroup() {
     this.#pageGroupElement.innerHTML = "";
 
-    const previousButton = document.createElement("button");
-    const previousList = document.createElement("li");
-    previousButton.textContent = "前へ";
-    previousButton.classList.add("previous");
-    previousButton.addEventListener("click", () => this.#handlePreviousClick());
-    previousList.appendChild(previousButton);
-    this.#pageGroupElement.appendChild(previousList);
+    const previousButtonItem = this.#createPreviousButtonItem();
+    this.#pageGroupElement.appendChild(previousButtonItem);
 
     const firstVisiblePageNumber = Math.min(
       Math.max(1, this.current - 4),
@@ -106,31 +101,30 @@ export class SpPagination extends HTMLElement {
       pageNumber <= lastVisiblePageNumber;
       pageNumber++
     ) {
-      const button = document.createElement("button");
-      const li = document.createElement("li");
-
-      button.textContent = String(pageNumber);
-      button.classList.add("page");
-
-      if (pageNumber === this.current) {
-        button.classList.add("selected");
-        button.setAttribute("aria-current", "page");
-      }
-
-      button.addEventListener("click", () => this.#handlePageClick(pageNumber));
-
-      li.appendChild(button);
-
-      this.#pageGroupElement.appendChild(li);
+      const pageButtonItem = this.#createPageButtonItem(pageNumber);
+      this.#pageGroupElement.appendChild(pageButtonItem);
     }
 
-    const nextButton = document.createElement("button");
-    const nextList = document.createElement("li");
-    nextButton.textContent = "次へ";
-    nextButton.classList.add("next");
-    nextButton.addEventListener("click", () => this.#handleNextClick());
-    nextList.appendChild(nextButton);
-    this.#pageGroupElement.appendChild(nextList);
+    const nextButtonItem = this.#createNextButtonItem();
+    this.#pageGroupElement.appendChild(nextButtonItem);
+  }
+
+  #createPageButtonItem(pageNumber: number) {
+    const button = document.createElement("button");
+    button.textContent = String(pageNumber);
+    button.classList.add("page");
+
+    if (pageNumber === this.current) {
+      button.classList.add("selected");
+      button.setAttribute("aria-current", "page");
+    }
+
+    button.addEventListener("click", () => this.#handlePageClick(pageNumber));
+
+    const li = document.createElement("li");
+    li.appendChild(button);
+
+    return li;
   }
 
   #handlePageClick(pageNumber: number) {
@@ -148,6 +142,18 @@ export class SpPagination extends HTMLElement {
     }
   }
 
+  #createPreviousButtonItem() {
+    const previousButton = document.createElement("button");
+    previousButton.textContent = "前へ";
+    previousButton.classList.add("previous");
+    previousButton.addEventListener("click", () => this.#handlePreviousClick());
+
+    const previousListItem = document.createElement("li");
+    previousListItem.appendChild(previousButton);
+
+    return previousListItem;
+  }
+
   #handlePreviousClick() {
     if (this.current > 1) {
       this.current--;
@@ -161,6 +167,18 @@ export class SpPagination extends HTMLElement {
 
       this.#updatePageGroup();
     }
+  }
+
+  #createNextButtonItem() {
+    const nextButton = document.createElement("button");
+    nextButton.textContent = "次へ";
+    nextButton.classList.add("next");
+    nextButton.addEventListener("click", () => this.#handleNextClick());
+
+    const nextListItem = document.createElement("li");
+    nextListItem.appendChild(nextButton);
+
+    return nextListItem;
   }
 
   #handleNextClick() {
