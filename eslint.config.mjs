0fce5b57a1afc28c29249d32142955842f944f4a
oki07 <takashi.oki@uzabase.com>
2024-12-31 23:35:19 +0900

    eslint-config-prettierを導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,4 +12,5 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettier,
 ];
