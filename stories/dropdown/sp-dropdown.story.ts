ddfdda3ff1c89ce1e16af8febe15a4c8907c0b2b
Ittetsu Sato <ittetsusato@gmail.com>
2025-01-02 11:45:24 +0900

    feat: スクロール

--- stories/dropdown/sp-dropdown.story.ts
+++ stories/dropdown/sp-dropdown.story.ts
@@ -15,6 +15,15 @@ const meta: Meta = {
       <sp-dropdown-option text="Text1Text1Text1Text1" value="Text1Text1Text1Text1"></sp-dropdown-option>
       <sp-dropdown-option text="Text2" value="Text2"></sp-dropdown-option>
       <sp-dropdown-option text="Text3" value="Text3"></sp-dropdown-option>
+      <sp-dropdown-option text="Text4" value="Text4"></sp-dropdown-option>
+      <sp-dropdown-option text="Text5" value="Text5"></sp-dropdown-option>
+      <sp-dropdown-option text="Text6" value="Text6"></sp-dropdown-option>
+      <sp-dropdown-option text="Text7" value="Text7"></sp-dropdown-option>
+      <sp-dropdown-option text="Text8" value="Text8"></sp-dropdown-option>
+      <sp-dropdown-option text="Text9" value="Text9"></sp-dropdown-option>
+      <sp-dropdown-option text="Text10" value="Text10"></sp-dropdown-option>
+      <sp-dropdown-option text="Text11" value="Text11"></sp-dropdown-option>
+
     </sp-dropdown>
   `,
 };
