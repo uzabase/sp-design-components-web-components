b29c95d5e27e82d95a1bb00a7ebd90c102e13496
GitHub Action <action@github.com>
2025-01-05 06:27:03 +0000

    distディレクトリを更新

---
+++ dist/components/notificationMessage/sp-notification-message.d.ts
@@ -0,0 +1,17 @@
+export type Type = "error" | "warning" | "information" | "success";
+export declare const iconPaths: Record<Type, string>;
+export declare class SpNotificationMessage extends HTMLElement {
+    #private;
+    get type(): Type;
+    set type(value: Type);
+    static get observedAttributes(): string[];
+    constructor();
+    connectedCallback(): void;
+    attributeChangedCallback(name: string, oldValue: string, newValue: string): void;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "sp-notification-message": SpNotificationMessage;
+    }
+}
+//# sourceMappingURL=sp-notification-message.d.ts.map
