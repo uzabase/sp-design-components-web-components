ac359b00df535fcae1ce002d0146cd7db8ce11b1
oki07 <takashi.oki@uzabase.com>
2025-02-25 23:52:14 +0900

    ストーリーのコードがより美しく見られる

--- stories/notificationBar/sp-notification-bar.story.ts
+++ stories/notificationBar/sp-notification-bar.story.ts
@@ -17,9 +17,9 @@ const meta = {
     slot: { type: "string" },
   },
   render: (args) => html`
-    <sp-notification-bar type=${ifDefined(args.type)}
-      >${args.slot}</sp-notification-bar
-    >
+    <sp-notification-bar type=${ifDefined(args.type)}>
+      ${args.slot}
+    </sp-notification-bar>
   `,
 } satisfies Meta<SpNotificationBar>;
 
@@ -49,13 +49,7 @@ export const AllTypes: Story = {
 
 export const LongText: Story = {
   args: {
-    slot: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-    est laborum.`,
+    slot: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 };
 
