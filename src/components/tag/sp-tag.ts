1acd417a758d0d26f1f72608c934fb6d1df8452c
oki07 <takashi.oki@uzabase.com>
2025-02-28 22:16:38 +0900

    color属性を実装（lightだけ）

--- src/components/tag/sp-tag.ts
+++ src/components/tag/sp-tag.ts
@@ -8,8 +8,25 @@ import tagStyle from "./tag.css?inline";
 const styles = new CSSStyleSheet();
 styles.replaceSync(`${resetStyle} ${foundationStyle} ${tagStyle}`);
 
+export type TagColor = "gray" | "green" | "red" | "yellow" | "blue";
+
+const colors: TagColor[] = ["gray", "green", "red", "yellow", "blue"];
+
+function isValidColor(value: string): value is TagColor {
+  return colors.some((color) => color === value);
+}
+
+const colorClasses: Record<TagColor, string> = {
+  gray: "color__gray",
+  green: "color__green",
+  red: "color__red",
+  yellow: "color__yellow",
+  blue: "color__blue",
+};
+
 export class SpTag extends HTMLElement {
   #removable = false;
+  #color: TagColor = "gray";
 
   get removable() {
     return this.#removable;
@@ -22,8 +39,19 @@ export class SpTag extends HTMLElement {
     this.#render();
   }
 
+  get color() {
+    return this.#color;
+  }
+
+  set color(value: TagColor) {
+    if (this.#color === value) return;
+
+    this.#color = value;
+    this.#render();
+  }
+
   static get observedAttributes() {
-    return ["removable"];
+    return ["removable", "color"];
   }
 
   constructor() {
@@ -38,14 +66,32 @@ export class SpTag extends HTMLElement {
 
   connectedCallback() {
     this.#removable = this.hasAttribute("removable");
+
+    const colorAttr = this.getAttribute("color");
+    if (colorAttr && isValidColor(colorAttr)) {
+      this.#color = colorAttr;
+    }
+
     this.#render();
   }
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
     if (oldValue === newValue) return;
 
-    if (name === "removable") {
-      this.removable = newValue === "true" || newValue === "";
+    switch (name) {
+      case "removable":
+        this.removable = newValue === "true" || newValue === "";
+        break;
+      case "color":
+        if (isValidColor(newValue)) {
+          this.color = newValue;
+        } else {
+          console.warn(
+            `${newValue}は無効なcolor属性です。デフォルト値"gray"を使用します。`,
+          );
+          this.color = "gray";
+        }
+        break;
     }
   }
 
@@ -54,6 +100,7 @@ export class SpTag extends HTMLElement {
 
     const baseElement = document.createElement("div");
     baseElement.classList.add("base");
+    baseElement.classList.add(colorClasses[this.#color]);
     baseElement.setAttribute("role", "tag");
 
     const contentElement = document.createElement("span");
